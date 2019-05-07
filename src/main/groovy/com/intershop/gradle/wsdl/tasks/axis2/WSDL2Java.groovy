c75851c0365b7f36ffbe4b9b348efecd5fd3110e
sromic <simun.romic@gmail.com>
2019-05-07 14:52:46 +0200

    ADD - support for gradle 4.X

--- src/main/groovy/com/intershop/gradle/wsdl/tasks/axis2/WSDL2Java.groovy
+++ src/main/groovy/com/intershop/gradle/wsdl/tasks/axis2/WSDL2Java.groovy
@@ -22,6 +22,7 @@ import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.Optional
+import org.gradle.process.internal.DefaultExecActionFactory
 import org.gradle.process.internal.JavaExecHandleBuilder
 
 class WSDL2Java extends AbstractWSDL2Java {
@@ -189,7 +190,7 @@ class WSDL2Java extends AbstractWSDL2Java {
      * @return JavaExecHandleBuilder
      */
     JavaExecHandleBuilder prepareExec() {
-        JavaExecHandleBuilder javaExec = new JavaExecHandleBuilder(getFileResolver())
+        JavaExecHandleBuilder javaExec = new DefaultExecActionFactory(getFileResolver()).newJavaExec()
         getJavaOptions().copyTo(javaExec)
 
         FileCollection axis2CodegenConfiguration = getProject().getConfigurations().getAt(WSDLExtension.WSDLAXIS2_CONFIGURATION_NAME)
